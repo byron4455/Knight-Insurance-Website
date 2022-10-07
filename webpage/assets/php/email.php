6c23201a155d9e4720f1c59cf5a569a427cd75d5
byron4455 <byronstephenson1@gmail.com>
2022-10-06 20:54:45 -0400

    added about pages and partners

---
+++ webpage/assets/php/email.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("byronstephenson1@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: byronstephenson1@gmail.com");
+
+
+}
+
+
+?>
